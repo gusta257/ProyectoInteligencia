2c01327a5be8189fb5e67e61bc217d22fadb2a55
gusta257 <gdeleontobias@gmail.com>
2020-05-21 22:49:21 -0600

    player ready

--- ClienteTotito.py
+++ ClienteTotito.py
@@ -1,5 +1,5 @@
 import socketio
-
+import random
 # standard Python
 sio = socketio.Client()
 
@@ -7,12 +7,12 @@ class Juego:
     def __init__(self):
         self.user_name = ""
         self.tournament_id = ""
-        self.user_role: 'player'
-
-
-@sio.on('my message')
-def on_message(data):
-    print('I received a message!')
+        self.ready = False
+        self.gameFinished = False
+        self.game_id = 0
+        self.player_turn_id = 0
+        self.winnerTurnID = 0
+        #user_role: "player"
 
 @sio.on('connect')
 def connect():
@@ -22,66 +22,54 @@ def connect():
     sio.emit('signin', {
         'user_name': juego.user_name,
         'tournament_id': juego.tournament_id,  
-        'user_role': juego.user_role
+        'user_role': "player"
+    })
+@sio.on('ready')
+def on_ready(data):
+    juego.gameFinished = False
+    juego.game_id = data['game_id']
+    juego.player_turn_id = data['player_turn_id']
+   
+    juego.ready = True
+
+    movement = []
+
+    direction = random.randint(0, 1)
+    position = random.randint(0, 29)
+
+    movement = [direction, position]
+    print("Movement played: " + str(movement[0]) + ", " + str(movement[1]))
+
+    sio.emit('play', {
+        'tournament_id': juego.tournament_id,
+        'player_turn_id': juego.player_turn_id,
+        'game_id': juego.game_id,
+        'movement': movement
+    })
+@sio.on('finish')
+def on_finish(data):
+    juego.game_id = data['game_id']
+    juego.player_turn_id = data['player_turn_id']
+    juego.winner_turn_id = data['winner_turn_id']
+    if data['player_turn_id'] == data['winner_turn_id']:
+        print("Eres el ganador")
+    else:
+        print("Perdiste")
+    print('Game finished!')
+    juego.gameFinished = True
+
+    sio.emit('player_ready', {
+        'tournament_id': juego.tournament_id,
+        'game_id': juego.game_id,
+        'player_turn_id': juego.player_turn_id
     })
 
-'''
-@sio.event
-def connect():
-    sio.emit('signin', {
-    'user_name': 'Gustavo Adolfo De Leon Tobias',
-    'tournament_id': 1000,
-    'user_role': 'player'})
-    print('connection established')
-
-
-@sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
-
-@sio.event
-def disconnect():
-    print('disconnected from server')
-
-@sio.on('ok_signin')
-def ok_signin():
-    print('Successfully signed in!')
-
-@sio.on('ready')
-def ready(data):
-    print('data of ready received', data)
-   
-@sio.on('finish')
-def finish(data):
-    print('tournament is over', data)
-'''
-#@sio.on('my message')
-#def on_message(data):
-#    print('I received a message!')
-#@sio.on('my message')
-#def on_message(data):
-#    print('I received a message!')
-
-#@sio.on('connect')
-#def connect():
-    # Client has connected
-#    print("Conectado: " + vm.user_name);
-    # Signing signal
-#    sio.emit('signin', {
-#        'user_name': vm.user_name,
-#        'tournament_id': vm.tournament_id,  
-#        'user_role': vm.user_role
-#    })
-
-
- 
 #sio.connect('http://3.12.129.126:4000')
 #sio.connect('http://localhost:4000')
 
 juego = Juego()
 juego.user_name = input("Ingrese su usuario: ")
-juego.tournament_id = input("Ingrese el Tournament ID: ")
+juego.tournament_id = int(input("Ingrese el Tournament ID: "))
 host = input("Ingrese el host: ")
 
 sio.connect(host)
